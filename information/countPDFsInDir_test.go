44174fa521fe53eb207b6b04cd75f64798d5dad5
Dr. Gernot Starke <gs@gernotstarke.de>
2021-01-08 16:29:02 +0100

    add some initial go files, q-and-dirty stuff

---
+++ information/countPDFsInDir_test.go
@@ -0,0 +1,45 @@
+package information
+
+import (
+	"pdfcpuSamples/samplepdfs"
+	"testing"
+)
+
+const prefix = samplepdfs.DirPrefix
+
+var testDirs = []struct {
+	dirName          string
+	expectedNrOfPDFs int
+}{
+	{ // empty Dir
+		prefix + "EmptyDir",
+		0,
+	},
+	{ // OnePDF
+		prefix + "OnePDF",
+		1,
+	},
+	{ // Four files, TWO PDFs
+		prefix + "FourFilesTwoPDFs",
+		2,
+	},
+	{ // 12 PDFfs
+		prefix + "TwelvePDFs",
+		12,
+	},
+	{ // One PDF, one subfolder with extension PDF that should not count
+		prefix + "OnePDFWithSubfolder",
+		1,
+	},
+}
+
+func TestCountPDFsInDir(t *testing.T) {
+
+	for _, d := range testDirs {
+		got := CountPDFsInDir(d.dirName)
+		if got != d.expectedNrOfPDFs {
+			t.Errorf("FAIL: directory %s expected %v, got %v PDF files", d.dirName, d.expectedNrOfPDFs, got)
+		}
+	}
+
+}
